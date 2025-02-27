b265fd8d22af1c372a5acfb2ef1ba3c79f6138c1
Tharindu Basnayaka <tharindupramuditha2@gmail.com>
2025-02-27 21:47:38 +0530

    created the first entity

---
+++ src/main/java/com/springdemo/springlearning/Student.java
@@ -0,0 +1,74 @@
+package com.springdemo.springlearning;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    private Integer id;
+
+    @Column(length = 30)
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Integer age, String email, String lastName, String firstName) {
+        this.age = age;
+        this.email = email;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+}
